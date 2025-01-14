108036d972daa42042be33ca33a76b33bbe88431
Hien Pham <hienduyph@gmail.com>
2025-01-14 10:03:35 +0700

    done vector builder

--- datatypes/src/main/java/dev/hienph/fusionj/datatypes/ArrowVectorBuilder.java
+++ datatypes/src/main/java/dev/hienph/fusionj/datatypes/ArrowVectorBuilder.java
@@ -1,7 +1,11 @@
 package dev.hienph.fusionj.datatypes;
 
-import org.apache.arrow.flatbuf.Null;
+import org.apache.arrow.vector.BigIntVector;
 import org.apache.arrow.vector.FieldVector;
+import org.apache.arrow.vector.Float4Vector;
+import org.apache.arrow.vector.Float8Vector;
+import org.apache.arrow.vector.IntVector;
+import org.apache.arrow.vector.SmallIntVector;
 import org.apache.arrow.vector.TinyIntVector;
 import org.apache.arrow.vector.VarCharVector;
 
@@ -21,16 +25,66 @@ public record ArrowVectorBuilder(FieldVector fieldVector) {
       return;
     }
     switch (fieldVector) {
-      case VarCharVector v -> v.set(i, value instanceof byte[] ? (byte[]) value : value.toString().getBytes());
+      case VarCharVector v ->
+          v.set(i, value instanceof byte[] ? (byte[]) value : value.toString().getBytes());
       case TinyIntVector v -> {
         if (value instanceof Number) {
           v.set(i, ((Number) value).byteValue());
         } else if (value instanceof String) {
-          v.set(i, Integer.parseInt((String)value));
-        } else  {
-          throw  new IllegalStateException();
+          v.set(i, Integer.parseInt((String) value));
+        } else {
+          throw new IllegalStateException();
         }
       }
+      case SmallIntVector v -> {
+        if (value instanceof Number) {
+          v.set(i, (short) value);
+        } else if (value instanceof String) {
+          v.set(i, Short.parseShort((String) value));
+        } else {
+          throw new IllegalStateException();
+        }
+      }
+      case IntVector v -> {
+        if (value instanceof Number) {
+          v.set(i, (int) value);
+        } else if (value instanceof String) {
+          v.set(i, Integer.parseInt((String) value));
+        } else {
+          throw new IllegalStateException();
+        }
+      }
+
+      case BigIntVector v -> {
+        if (value instanceof Number) {
+          v.set(i, (long) value);
+        } else if (value instanceof String) {
+          v.set(i, Long.parseLong((String) value));
+        } else {
+          throw new IllegalStateException();
+        }
+      }
+
+      case Float4Vector v -> {
+        if (value instanceof Number) {
+          v.set(i, (float) value);
+        } else if (value instanceof String) {
+          v.set(i, Float.parseFloat((String) value));
+        } else {
+          throw new IllegalStateException();
+        }
+      }
+
+      case Float8Vector v -> {
+        if (value instanceof Number) {
+          v.set(i, (double) value);
+        } else if (value instanceof String) {
+          v.set(i, Double.parseDouble((String) value));
+        } else {
+          throw new IllegalStateException();
+        }
+      }
+
       default -> throw new IllegalStateException(fieldVector.getClass().getName());
     }
   }
--- datatypes/src/main/java/dev/hienph/fusionj/datatypes/RecordBatch.java
+++ datatypes/src/main/java/dev/hienph/fusionj/datatypes/RecordBatch.java
@@ -2,16 +2,17 @@ package dev.hienph.fusionj.datatypes;
 
 import java.util.List;
 
-public record RecordBatch(Schema schema, List<ColumnVector> fields) {
+public record RecordBatch(Schema schema, List<? extends ColumnVector> fields) {
+
   public Integer rowCount() {
-    return  fields.getFirst().size();
+    return fields.getFirst().size();
   }
 
-  public  Integer columnCount() {
+  public Integer columnCount() {
     return fields.size();
   }
 
-  public  ColumnVector field(Integer i) {
+  public ColumnVector field(Integer i) {
     return fields.get(i);
   }
 
@@ -20,11 +21,11 @@ public record RecordBatch(Schema schema, List<ColumnVector> fields) {
     return toCSV();
   }
 
-  public  String toCSV() {
+  public String toCSV() {
     var sb = new StringBuilder();
     final var columnCount = schema.fields().size();
     for (var rowIndex = 0; rowIndex < rowCount(); rowIndex++) {
-      for (var columnIndex = 0; columnIndex < columnCount; columnIndex ++) {
+      for (var columnIndex = 0; columnIndex < columnCount; columnIndex++) {
         if (columnIndex > 0) {
           sb.append(",");
         }
